b80eb4fa5615a8a5a410f5b46a95cdaa0ad36d7c
Dariya Vedernikova <daria-vedernikova08@yandex.ru>
2024-05-19 22:34:27 +0300

    fix: change cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,14 +1,16 @@
 const allowedCors = [
-    "https://pindie-app.nomoredomainswork.ru"
+    "https://pindie-app.nomoredomainswork.ru",
+    "http://localhost:3000",
+    "https://server.nomoredomainswork.ru"
 ];
 
 const cors = (req, res, next) => {
     const {origin} = req.headers;
     if (allowedCors.includes(origin)) {
         res.header("Access-Control-Allow-Origin", origin);
-        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-        res.header("Access-Control-Allow-Credentials", "true");
+        res.header("Access-Control-Allow-Credentials", true);
     }
 
     if (req.method === 'OPTIONS') {
