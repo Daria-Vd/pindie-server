32aeca61efae3d43afad7ff4e4836f1458731843
Dariya Vedernikova <daria-vedernikova08@yandex.ru>
2024-05-19 21:57:57 +0300

    fix: change cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,21 +2,16 @@ const allowedCors = [
     "https://pindie-app.nomoredomainswork.ru"
 ];
 
-const cors = (req, res, next) => {
+function cors(req, res, next) {
     const {origin} = req.headers;
+
     if (allowedCors.includes(origin)) {
         res.header("Access-Control-Allow-Origin", origin);
-        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-        res.header("Access-Control-Allow-Credentials", "true");
     }
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+    next();
+}
 
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-};
-
-module.exports = {cors};
+module.exports = cors;
 
